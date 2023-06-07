5181d5e6087b85a725a37105cac2f9f5e73e29a2
aurona.23 <auronagashi@hotmail.com>
2023-06-07 08:45:11 +0200

    nuovo sorting

--- Projects/Raffinamento/src/sorting.hpp
+++ Projects/Raffinamento/src/sorting.hpp
@@ -56,6 +56,28 @@ namespace SortingLibrary
       }
       return vSorted;
   }
+
+
+  template<typename T>
+  vector<T> InsertionSort(const vector<T>& v){
+    unsigned int n = v.size();
+    vector<T> sortedV = v;
+    for(unsigned int i = 0; i < n; i++)
+    {
+      T next = sortedV[i];
+      unsigned int j = i;
+
+      while((j > 0 ) && (sortedV[j-1] < next))
+      {
+        sortedV[j] = sortedV[j-1];
+        j--;
+      }
+      sortedV[j] = next;
+    }
+
+    return sortedV;
+  }
+
 }
 
 #endif // __SORTING_H
